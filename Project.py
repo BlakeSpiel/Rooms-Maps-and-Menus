bcfa3aadee6a09fa3dfcb22ac16cfef7e9993a2b
Blake <183446547+BlakeSpiel@users.noreply.github.com>
2024-10-02 11:16:25 -0500

    Create Project.py

---
+++ Project.py
@@ -0,0 +1,30 @@
+import turtle
+screen = turtle.Screen()
+screen.title("Finding Bob")
+
+t = turtle.Turtle()
+t.speed(0)
+t.hideturtle()
+
+
+#Functions for the different rooms
+def frontCemetery():
+  
+def backCemetery():
+
+def church():
+
+def entranceCatacombs():
+
+def eastCatacombs():
+
+
+
+
+
+
+
+
+
+
+turtle.mainloop()
