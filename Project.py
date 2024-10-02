dad0edc40acefc9620b7db7395dbe9f6baa4dc3a
Blake <183446547+BlakeSpiel@users.noreply.github.com>
2024-10-02 11:49:38 -0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,6 +1,7 @@
 import turtle
 screen = turtle.Screen()
 screen.title("Finding Bob")
+screen.setup(width=800, height=600)
 
 t = turtle.Turtle()
 t.speed(0)
@@ -8,18 +9,47 @@ t.hideturtle()
 
 
 #Functions for the different rooms
-def frontCemetery():
+#def frontCemetery():
   
-def backCemetery():
+  
+def cemetery():
+  t.penup()
+  t.goto(0, 200)
+  t.pendown()
+  t.left(108)
+  for _ in range(2):
+    t.forward(200)
+    t.right(90)
+    t.forward(200)
+    t.right(90)
 
 def church():
+  t.penup()
+  t.goto(100, 0)
+  t.pendown()
+  t.goto(200, -50)
+  t.pendown()
+  t.right(108)
+  for _ in range(5):
+    t.forward(70)
+    t.right(72)
+    t.forward(70)
+    t.left(144)
 
-def entranceCatacombs():
+    print("""
+      “The door creaks as you enter the church, 
+      and the moon lights up the inside of the building 
+      through the cracks in the roof. It looks as if the building 
+      could fall apart at any moment.”
+    """)
 
-def eastCatacombs():
+#def entranceCatacombs():
+
+#def eastCatacombs():
 
 
-
+church()
+cemetery()
 
 
 
